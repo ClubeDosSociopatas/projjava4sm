4ed808ee311f4fc7bb85aeae32bf1b6a1be842b9
Rafael Selner <7202408-Rafarim@users.noreply.gitlab.com>
2021-10-15 14:51:16 -0300

    Funcao do email funcionando?

--- src/server/Controlador.java
+++ src/server/Controlador.java
@@ -4,6 +4,10 @@ import java.security.NoSuchAlgorithmException;
 import java.security.SecureRandom;
 import java.math.BigInteger; 
 import java.nio.charset.StandardCharsets;
+import javax.mail.*;
+import javax.mail.internet.InternetAddress;
+import javax.mail.internet.MimeMessage;
+import java.util.Properties;
 
 public class Controlador {
     private Banco bd = new Banco();
@@ -105,5 +109,47 @@ public class Controlador {
         int dig2 = (11 - (soma % 11));
         if(!(((dig2 == 10 || dig2 == 11)&& Character.getNumericValue(input.charAt(10)) == 0) || dig2 == Character.getNumericValue(input.charAt(10)))){return true;}
         return false;
+    }
+
+    private void enviarEmail(String emailD, String assunto, String conteudo){
+        final String username = "rafazeteste@gmail.com";
+        final String password = "TeStandoHU1234";
+
+        Properties prop = new Properties();
+		prop.put("mail.smtp.host", "smtp.gmail.com");
+        prop.put("mail.smtp.port", "587");
+        prop.put("mail.smtp.auth", "true");
+        prop.put("mail.smtp.starttls.enable", "true"); //TLS
+        
+        Session session = Session.getInstance(prop,
+                new javax.mail.Authenticator() {
+                    protected PasswordAuthentication getPasswordAuthentication() {
+                        return new PasswordAuthentication(username, password);
+                    }
+                });
+
+        try {
+
+            Message message = new MimeMessage(session);
+            message.setFrom(new InternetAddress("rafazeteste@gmail.com"));
+            message.setRecipients(
+                    Message.RecipientType.TO,
+                    InternetAddress.parse(emailD)
+            );
+            message.setSubject(assunto);
+            message.setText(conteudo);
+
+            Transport.send(message);
+
+            System.out.println("Done");
+
+        } catch (MessagingException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static void main(String[] args) {
+        Controlador ctt = new Controlador();
+        ctt.enviarEmail("rafaselner@hotmail.com", "testeJava", "Esse e um teste de JavaMail");
     }
 }
